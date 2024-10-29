17dafc1129f3477290d09027a82b9ef4fba3a51c
fakumetal <faku.metal@gmail.com>
2024-10-29 12:00:33 -0300

    .

--- netlify/functions/payment_status.js
+++ netlify/functions/payment_status.js
@@ -1,25 +1,25 @@
-// Usa import en lugar de require
-export async function handler(event) {
-    const paymentId = event.queryStringParameters.id;
+// // Usa import en lugar de require
+// export async function handler(event) {
+//     const paymentId = event.queryStringParameters.id;
 
-    try {
-        const response = await fetch(`https://api.mercadopago.com/v1/payments/${paymentId}`, {
-            headers: {
-                Authorization: `Bearer APP_USR-5750875078357351-102414-7b50697d9dc9b3998b7aa0ee104a9589-2055936542`, // Reemplaza con tu Access Token de MercadoPago
-            },
-        });
+//     try {
+//         const response = await fetch(`https://api.mercadopago.com/v1/payments/${paymentId}`, {
+//             headers: {
+//                 Authorization: `Bearer  token aqui`,  
+//             },
+//         });
 
-        const paymentData = await response.json();
+//         const paymentData = await response.json();
 
-        return {
-            statusCode: 200,
-            body: JSON.stringify(paymentData),
-        };
-    } catch (error) {
-        console.error("Error fetching payment status: ", error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ message: "Error fetching payment status" }),
-        };
-    }
-}
+//         return {
+//             statusCode: 200,
+//             body: JSON.stringify(paymentData),
+//         };
+//     } catch (error) {
+//         console.error("Error fetching payment status: ", error);
+//         return {
+//             statusCode: 500,
+//             body: JSON.stringify({ message: "Error fetching payment status" }),
+//         };
+//     }
+// }
--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -2,13 +2,23 @@
 
 import { Footer, TopNavigation } from '@/components/layout';
 import { PaymenSuccessPage } from '@/components/pages';
+import React, { Suspense } from 'react';
+
+// Componente de retroceso
+const Loading = () => (
+  <div style={{ textAlign: 'center', padding: '20px' }}>
+    <h2>Cargando...</h2>
+  </div>
+);
 
 const Reservas: React.FC = () => {
   return (
-    <div  >
-      <TopNavigation />
-      <PaymenSuccessPage />
-      <Footer />
+    <div>
+      {/* <TopNavigation />
+      <Suspense fallback={<Loading />}>
+        <PaymenSuccessPage />
+      </Suspense>
+      <Footer /> */}
     </div>
   );
 };
